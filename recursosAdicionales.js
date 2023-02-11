d0a848102394e22e86b3546a1a40e33eb4afe196
Amelissarc <melissa.rinconft@gmail.com>
2023-02-11 12:23:07 -0300

    recursosAdicionales

---
+++ recursosAdicionales.js
@@ -0,0 +1,17 @@
+/* 
+Expresión Switch
+La expresión Switch evalúa una condición. Compara su valor con una instancia case, 
+y ejecuta declaraciones asociadas a ese case, así como las declaraciones en los case que siguen.
+
+Expresión Switch
+La expresión Switch evalúa una condición. Compara su valor con una instancia case, 
+y ejecuta declaraciones asociadas a ese case, así como las declaraciones en los case que siguen.
+
+Expresión Switch
+La expresión Switch evalúa una condición. Compara su valor con una instancia case, 
+y ejecuta declaraciones asociadas a ese case, así como las declaraciones en los case que siguen.
+
+Expresión Switch
+La expresión Switch evalúa una condición. Compara su valor con una instancia case, 
+y ejecuta declaraciones asociadas a ese case, así como las declaraciones en los case que siguen.
+*/
